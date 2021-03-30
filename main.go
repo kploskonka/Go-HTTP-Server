05560280f1cca6164e1d8de09b2ce1bf0f7042d0
Katarzyna Płoskonka <kasplosk@gmail.com>
2021-03-30 17:42:49 +0200

    Run HTTP Server serving given html file

--- main.go
+++ main.go
@@ -3,10 +3,31 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
 const version = "0.0.1"
+
+type FileHandler struct {
+    fileName string
+}
+
+func (fh *FileHandler) renderFile(res http.ResponseWriter, req *http.Request) {
+	body, _ := ioutil.ReadFile(fh.fileName)
+
+	fmt.Fprint(res, string(body))
+}
+
+func runServer(fileName string) {
+	fmt.Printf("HTTP Server with %v is running on http://localhost:8090/. Press Ctrl+C to stop.\n", fileName)
+
+	fileHandler := &FileHandler{fileName: fileName}
+
+	http.HandleFunc("/", fileHandler.renderFile)
+	http.ListenAndServe(":8090", nil)
+}
 
 func main() {
 	runCommand := flag.NewFlagSet("run", flag.ExitOnError)
@@ -25,5 +46,6 @@ func main() {
 	case "run":
 		runCommand.Parse(os.Args[2:])
 		fmt.Printf("Ran command 'run --file %v'\n", *fileName)
+		runServer(*fileName)
 	}
 }
